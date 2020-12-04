c6ea77ba5bd72a5fed62ec15b440732c9f3d2c9c
Ryan Walker <ryanwalker.design@gmail.com>
2020-12-03 22:39:39 -0500

    removed gitignored dirs

--- dist/executeVersion.d.ts
+++
@@ -1,3 +0,0 @@
-import { Types } from "@graphql-codegen/plugin-helpers";
-import { GraphQLSchema } from "graphql";
-export declare const plugin: (schema: GraphQLSchema, documents: Types.DocumentFile[], config?: object) => string;
